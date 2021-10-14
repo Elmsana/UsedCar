372309156030d3de8249f2f1ed798f7e7caa9c8c
amanuel-t <git>
2021-10-14 13:09:04 -0400

    used car

--- src/test/java/com/cars/tests/UsedCarTest.java
+++ src/test/java/com/cars/tests/UsedCarTest.java
@@ -8,21 +8,22 @@ import com.cars.utilities.Driver;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class UsedCarTest {
+
     @BeforeMethod
-    public void OpenTests(){
+    public void OpenTests() {
         Driver.get().get(ConfigurationReader.get("url"));
-        //SearchToolPage selectFilter = new SearchToolPage();
-        System.out.println(Driver.get().getTitle());
+        Driver.get().manage().deleteAllCookies();
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
     }
+
     @Test
     public void UsedCarSearch() {
         SearchToolPage selectFilter = new SearchToolPage();
@@ -33,16 +34,14 @@ public class UsedCarTest {
         selectFilter.noMaxPrice.click();
         selectFilter.noMilesFrom.click();
         selectFilter.zip.click();
-        BrowserUtil.waitFor(3);
         selectFilter.zip.clear();
         selectFilter.zip.sendKeys("60008");
         selectFilter.search.click();
-
-        BrowserUtil.waitFor(5);
         BrowserUtil.verifyElementDisplayed(selectFilter.MaxPriceFilter);
         BrowserUtil.verifyElementDisplayed(selectFilter.HondaFilter);
         BrowserUtil.verifyElementDisplayed(selectFilter.PilotFilter);
         BrowserUtil.verifyElementDisplayed(selectFilter.UsedFilter);
+
         BrowserUtil.ScrollDown(selectFilter.NewAndUsedFilter);
         selectFilter.newRadioButton.click();
         BrowserUtil.ScrollUp(selectFilter.newRadioButton);
@@ -57,26 +56,29 @@ public class UsedCarTest {
         BrowserUtil.clickWithJS(selectFilter.selectSecondCar);
         BrowserUtil.doubleClick(selectFilter.selectSecondCar);
 
-        Driver.get().getTitle().equalsIgnoreCase("Honda Pilot 8-Passenger For Sale");
-        BrowserUtil.waitFor(5);
-        BrowserUtil.clickWithJS(selectFilter.checkAvailability1);
-        selectFilter.checkAvailability.click();
-        Driver.get().manage().deleteAllCookies();
-        BrowserUtil.clickWithTimeOut(selectFilter.firsName, 3);
-        BrowserUtil.doubleClick(selectFilter.firsName);
-        selectFilter.firsName.click();
-        selectFilter.firsName.sendKeys("car");
-        BrowserUtil.clickWithTimeOut(selectFilter.lastName, 3);
-        selectFilter.lastName.click();
-        selectFilter.lastName.sendKeys("Owner");
-        BrowserUtil.clickWithTimeOut(selectFilter.email, 3);
-        selectFilter.email.click();
-        selectFilter.email.sendKeys("carowner@yahoo.com");
+
+
+            Driver.get().getTitle().equalsIgnoreCase("Honda Pilot 8-Passenger For Sale");
+            BrowserUtil.clickWithJS(selectFilter.checkAvailability1);
+            selectFilter.checkAvailability.click();
+            Driver.get().manage().deleteAllCookies();
+            BrowserUtil.clickWithTimeOut(selectFilter.firsName, 3);
+            BrowserUtil.doubleClick(selectFilter.firsName);
+            selectFilter.firsName.click();
+            selectFilter.firsName.sendKeys("car");
+            BrowserUtil.clickWithTimeOut(selectFilter.lastName, 3);
+            selectFilter.lastName.click();
+            selectFilter.lastName.sendKeys("Owner");
+            BrowserUtil.clickWithTimeOut(selectFilter.email, 3);
+            selectFilter.email.click();
+            selectFilter.email.sendKeys("carowner@yahoo.com");
         BrowserUtil.takeSnapShot(selectFilter.paymentCalculator, "aman.png");
-    }
-    @AfterMethod
-        public void close(){
-            Driver.closeDriver();
         }
 
+
+@AfterMethod
+    public void close() {
+        Driver.closeDriver();
     }
+
+}
